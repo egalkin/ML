f215e2777f38106095276ba0e96e72b83a1cb5f8
Egor Galkin <egalkin@devexperts.com>
2019-11-17 16:53:55 +0300

    Add normalization

--- kNN/knn.py
+++ kNN/knn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import nonparametric_regression as nr
 import matplotlib.pyplot as plt
 import f_measure as fm
+from sklearn import preprocessing
 
 STEP_DIVIDER = 500.0
 
@@ -16,44 +17,44 @@ def preprocess(classes_distribution, data_set):
             classes_distribution[cls] = [i]
 
 
-def parzen_window(data_set, excluded_idx, classes_distribution, classes_to_numbers, q, distance_func, kernel_func, h):
+def parzen_window(X, excluded_idx, classes_distribution, classes_to_numbers, q, distance_func, kernel_func, h):
     classes_results = [0] * len(classes_distribution.keys())
     for cls in classes_distribution.keys():
         cls_counted_function = 0
         for i in classes_distribution[cls]:
             if i != excluded_idx:
-                cls_counted_function += kernel_func(nr.div(distance_func(data_set[i][:-1], q), h))
+                cls_counted_function += kernel_func(nr.div(distance_func(X[i], q), h))
         classes_results[classes_to_numbers[cls]] = cls_counted_function
     return classes_results.index(max(classes_results))
 
 
-def count_distances(distances, data_set, distance_func):
+def count_distances(distances, X, distance_func):
     max_dist = 0
-    for i in range(len(data_set)):
-        for j in range(len(data_set)):
+    for i in range(len(X)):
+        for j in range(len(X)):
             if i != j:
-                cur_dist = distance_func(data_set[i][:-1], data_set[j][:-1])
+                cur_dist = distance_func(X[i], X[j])
                 distances[i].append(cur_dist)
                 max_dist = max(cur_dist, max_dist)
         distances[i].sort()
     return max_dist
 
 
-def test_parameters(data_set, nearest_neighbours, num_of_classes, classes_distribution, classes_to_numbers,
+def test_parameters(X, Y, nearest_neighbours, num_of_classes, classes_distribution, classes_to_numbers,
                     distance_func,
                     kernel_func, k, is_fixed_window):
     confusion_matrix = np.zeros((num_of_classes, num_of_classes), dtype=np.int)
-    for i in range(len(data_set)):
-        xi = data_set[i][:-1]
-        yi = classes_to_numbers[data_set[i][-1]]
+    for i in range(len(X)):
+        xi = X[i]
+        yi = classes_to_numbers[Y[i]]
         h = k if is_fixed_window else nearest_neighbours[i][k]
-        ai = parzen_window(data_set, i, classes_distribution, classes_to_numbers, xi, distance_func,
+        ai = parzen_window(X, i, classes_distribution, classes_to_numbers, xi, distance_func,
                            kernel_func, h)
         confusion_matrix[ai][yi] += 1
     return fm.count_micro_f_measure(confusion_matrix, num_of_classes)
 
 
-def window_regression(data_set, classes_distribution, is_fixed_window):
+def window_regression(X, Y, classes_distribution, is_fixed_window):
     distances = nr.Distances.__dict__.copy()
     kernels = nr.Kernels.__dict__.copy()
     removed_fields = ['__module__', '__dict__', '__weakref__', '__doc__']
@@ -69,14 +70,14 @@ def window_regression(data_set, classes_distribution, is_fixed_window):
     best_values = (None, None, 0, 0)
     for distance_func_name in distances.keys():
         distance_func = distances[distance_func_name].__func__
-        nearest_neighbours = [[] for i in range(len(data_set))]
-        max_dist = count_distances(nearest_neighbours, data_set, distance_func)
+        nearest_neighbours = [[] for i in range(len(X))]
+        max_dist = count_distances(nearest_neighbours, X, distance_func)
         windows_step = max_dist / STEP_DIVIDER
-        window_range = np.arange(windows_step, max_dist, windows_step) if is_fixed_window else range(len(data_set) - 1)
+        window_range = np.arange(windows_step, max_dist, windows_step) if is_fixed_window else range(len(X) - 1)
         for kernel_func_name in kernels.keys():
             kernel_func = kernels[kernel_func_name].__func__
             for k in window_range:
-                cur_f_measure = test_parameters(data_set, nearest_neighbours, num_of_classes, classes_distribution,
+                cur_f_measure = test_parameters(X, Y, nearest_neighbours, num_of_classes, classes_distribution,
                                                 classes_to_numbers,
                                                 distance_func, kernel_func, k, is_fixed_window)
                 if cur_f_measure > f_measure:
@@ -88,20 +89,20 @@ def window_regression(data_set, classes_distribution, is_fixed_window):
 # ('euclidean', 'sigmoid', 0.12036968843043502, 0.7575095531167202) - fixed
 # ('manhattan', 'triweight', 11, 0.7599672025991207) - variable
 
-def build_graphics(data_set, classes_distribution, distance_name, kernel_name, is_fixed_window):
+def build_graphics(X, Y, classes_distribution, distance_name, kernel_name, is_fixed_window):
     distance_func = nr.Distances.__dict__[distance_name].__func__
     kernel_func = nr.Kernels.__dict__[kernel_name].__func__
     num_of_classes = len(classes_distribution.keys())
     classes_to_numbers = {}
     for idx, cl in enumerate(classes_distribution.keys()):
         classes_to_numbers[cl] = idx
-    nearest_neighbours = [[] for i in range(len(data_set))]
-    max_dist = count_distances(nearest_neighbours, data_set, distance_func)
+    nearest_neighbours = [[] for i in range(len(X))]
+    max_dist = count_distances(nearest_neighbours, X, distance_func)
     windows_step = max_dist / STEP_DIVIDER
-    window_range = np.arange(windows_step, max_dist, windows_step) if is_fixed_window else range(len(data_set) - 1)
+    window_range = np.arange(windows_step, max_dist, windows_step) if is_fixed_window else range(len(X) - 1)
     f_measure_values = []
     for k in window_range:
-        f_measure = test_parameters(data_set, nearest_neighbours, num_of_classes, classes_distribution,
+        f_measure = test_parameters(X, Y, nearest_neighbours, num_of_classes, classes_distribution,
                                     classes_to_numbers,
                                     distance_func, kernel_func, k, is_fixed_window)
         f_measure_values.append(f_measure)
@@ -116,10 +117,13 @@ def solve():
     data_set = pd.read_csv('dataset_41_glass.csv')
     classes_distribution = {}
     preprocess(classes_distribution, data_set)
-    # build_graphics(data_set.values, classes_distribution, 'euclidean', 'sigmoid', True)
-    build_graphics(data_set.values, classes_distribution, 'manhattan', 'triweight', False)
+    X = data_set.values[:, :-1]
+    Y = data_set.values[:, -1]
+    normalized_X = preprocessing.normalize(X)
+    build_graphics(normalized_X, Y, classes_distribution, 'euclidean', 'sigmoid', True)
+    # build_graphics(normalized_X, Y, classes_distribution, 'manhattan', 'triweight', False)
     # print(datetime.now())
-    # print(window_regression(data_set.values, classes_distribution, True))
+    # print(window_regression(normalized_X, Y, classes_distribution, True))
     # print(datetime.now())
     # print(datetime.now())
     # print(window_regression(data_set.values, classes_distribution, False))
